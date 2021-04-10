2a2d60c2b38d987118b493451b6cd504736d1a2e
lethetiem <19522330@gm.uit.edu.vn>
2021-04-10 08:46:13 +0700

    Tao giao dien login

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,18 +1,22 @@
 package app;
 
 import javafx.application.Application;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.scene.control.Button;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("Views/sample_view.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setTitle("Login page");
+        primaryStage.setScene(new Scene(root, 690, 540));
+
+
         primaryStage.show();
     }
 
