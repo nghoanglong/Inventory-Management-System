57abfeb6933af112a2134f7878318e75b97bd502
hoanglong <19521788@gm.uit.edu.vn>
2021-04-17 10:09:59 +0700

    Hoan thanh connect login va signup

--- src/Controllers/LoginController.java
+++ src/Controllers/LoginController.java
@@ -2,15 +2,14 @@
 package Controllers;
 
 // Import JavaFX libs
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Alert;
-import javafx.scene.control.Button;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.stage.Stage;
 import javafx.event.ActionEvent;
 
@@ -31,6 +30,17 @@ public class LoginController
     private Button signUpButton;
     @FXML
     private Button loginButton;
+    @FXML
+    private Label noticeLabel;
+
+    @FXML
+    public void initialize()
+    {
+        noticeLabel.setVisible(false);
+    }
+
+    // Tìm hiểu REGEX để kiếm tra input data - nhập quá 100 ký tự
+
 
     // Set action for Login Button
     public void loginButtonAction(ActionEvent event)
@@ -46,17 +56,28 @@ public class LoginController
         LOGIN_MDW new_login = new LOGIN_MDW();
         int check_result = new_login.validate_login(userName, passWord);
 
-        // Define Alert virable to make pop up alert
-        Alert alert_check_result = new Alert(Alert.AlertType.INFORMATION);
-        alert_check_result.setContentText("Check result = " + check_result);
+        switch (check_result)
+        {
+            case 1:
+                noticeLabel.setText("Login success");
+                noticeLabel.setVisible(true);
+                break;
+            case 2:
+                noticeLabel.setText("User not exist");
+                noticeLabel.setVisible(true);
+                break;
+            case 3:
+                noticeLabel.setText("Wrong password");
+                noticeLabel.setVisible(true);
+                break;
+        }
 
-        // Show data input from user
-        alert_check_result.show();
+        System.out.println(noticeLabel);
     }
 
     public void signUpButtonAction(ActionEvent event) throws IOException
     {
-        Parent signUpParent = FXMLLoader.load(getClass().getClassLoader().getResource("Views/Sign_Up_Scene.fxml"));
+        Parent signUpParent = FXMLLoader.load(getClass().getClassLoader().getResource("Views/SignUp/signup.fxml"));
         Stage signUpStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         Scene signUpScene = new Scene(signUpParent);
 
