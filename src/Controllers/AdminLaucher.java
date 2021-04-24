37ad72a898654dbac890174087908206cd435ba1
Nguyễn Dương Tùng <19522494@gm.uit.edu.vn>
2021-04-24 21:56:02 +0700

    fixed launcher

--- src/Controllers/AdminLaucher.java
+++ src/Controllers/AdminLaucher.java
@@ -1,2 +1,22 @@
-package Controllers;public class AdminLaucher {
+package Controllers;
+
+import Models.USERS;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.event.ActionEvent;
+import javafx.stage.Stage;
+
+public class AdminLaucher {
+    @FXML
+    private Button productBTN;
+
+    @FXML
+    public void initialize()    {
+        //productBTN.setStyle("-fx-background-image: url('/Views/AdminLauncher/customer.png')");
+    }
+
 }
