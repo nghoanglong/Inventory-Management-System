974c98f9a968bc2df49a4234984c646769b5ea0e
Nguyễn Dương Tùng <19522494@gm.uit.edu.vn>
2021-05-09 11:32:44 +0700

    add product management view

---
+++ src/Controllers/ProductManagement.java
@@ -0,0 +1,21 @@
+package Controllers;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.layout.VBox;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ProductManagement implements Initializable {
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+    // Không sử dụng table view mà sử dụng Vbox và đổ Items vào Vbox
+    // Ở view Item có click handler -> khi nhấn vào một item -> các giá trị của scene item sẽ trả về cho scene management
+
+}
