5038d2056390347b0621147666017592ff3eb62b
chuanchon <sonchu@chuanchons-MacBook-Pro.local>
2021-05-18 18:07:59 +0700

    update button Xoa va TroVe

--- src/Controllers/CartController.java
+++ src/Controllers/CartController.java
@@ -5,21 +5,27 @@ import Controllers.ProductManagement.SANPHAM;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.stage.Stage;
 
 import java.awt.event.ActionEvent;
+import java.io.IOException;
 
 public class CartController {
     @FXML
-    private TextField tenkh_tf;
+    private TextField tenkhTF;
     @FXML
-    private TextField sdtkh_tf;
+    private TextField sdtkhTF;
     @FXML
-    private TextField diachikh_tf;
+    private TextField diachikhTF;
 
     @FXML
-    private TableView<SANPHAM> chitietyc_tv;
+    private TableView<SANPHAM> chitietycTV;
     @FXML
     private TableColumn<SANPHAM,String> tenspCol;
     @FXML
@@ -31,11 +37,13 @@ public class CartController {
 
 
     @FXML
-    private Button ycnhap_btn;
+    private Button ycnhapBtn;
     @FXML
-    private Button ycxuat_btn;
+    private Button ycxuatBtn;
     @FXML
-    private Button clear_btn;
+    private Button xoaspBtn;
+    @FXML
+    private Button backBtn;
 
     //Class Variable
     private ObservableList<SANPHAM> data;
@@ -44,7 +52,7 @@ public class CartController {
     public void initialize(){
         data = FXCollections.observableArrayList(ProductManagementController.lisp_yc);
         this.initTable();
-        this.chitietyc_tv.setItems(data);
+        this.chitietycTV.setItems(data);
     }
 
     public void initTable(){
@@ -53,13 +61,29 @@ public class CartController {
         giaspCol.setCellValueFactory(new PropertyValueFactory<SANPHAM, Integer>("gia_sp"));
         numspCol.setCellValueFactory(new PropertyValueFactory<SANPHAM, Integer>("num_sp"));
     }
-
-    public void NhapBtnAction(ActionEvent e){
+    public void ycnhapBtnAction(ActionEvent e){
         // Gom cục data thành 1 đơn hàng và insert vô table YEUCAU để đợi Admin và WHManager phê duyệt
+
+        // Sau khi thực hiện tạo đơn hàng thành công thì xóa dữ liệu đang lưu trong ArrayList tạm
+        ProductManagementController.lisp_yc.removeAll(data);
     }
-
-    public void XuatBtnAction(ActionEvent e){
+    public void ycxuatBtnAaction(ActionEvent e){
         // Gom cục data thành 1 đơn hàng và insert vô table YEUCAU để đợi Admin và WHManager phê duyệt
+
+        // Sau khi thực hiện tạo đơn hàng thành công thì xóa dữ liệu đang lưu trong ArrayList tạm
+        ProductManagementController.lisp_yc.removeAll(data);
+    }
+    public void backBtnAaction(ActionEvent e) throws IOException {
+        Parent homeParent = FXMLLoader.load(getClass().getClassLoader().getResource("Views/HomeScreen/WarehouseLauncher/warehouse_launcher.fxml"));
+        Stage homeStage = (Stage) ((Node) e.getSource()).getScene().getWindow();
+        Scene homeScene = new Scene(homeParent);
+
+        homeStage.setScene(homeScene);
+        homeStage.show();
+    }
+    public void xoaBtnAction(ActionEvent e){
+        SANPHAM selected = chitietycTV.getSelectionModel().getSelectedItem();
+        ProductManagementController.lisp_yc.remove(selected);
     }
 }
 
